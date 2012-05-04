28f9c231db80cea7b709d6e296eaf708248a487a
ttsuruoka <tatsuya.tsuruoka@gmail.com>
2012-05-04 12:44:56 +0900

    fixed require path

--- script/bootstrap.php
+++ script/bootstrap.php
@@ -3,3 +3,6 @@ define('ROOT_DIR', dirname(__DIR__).'/');
 define('APP_DIR', ROOT_DIR.'app/');
 require_once ROOT_DIR.'dietcake/dietcake.php';
 require_once dirname(__DIR__).'/app/config/bootstrap.php';
+
+require_once VENDOR_DIR.'SimpleAmazonDynamoDB/SimpleAmazonSTS.php';
+require_once VENDOR_DIR.'SimpleAmazonDynamoDB/SimpleAmazonDynamoDB.php';
